1cbc885ccf717c4acbbf0fdec0a7ad82c1a09e22
Abyasa Bazla <abyasabazla@gmail.com>
2025-06-02 10:09:14 +0700

    Initial commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,46 @@
+import type { z } from 'astro/zod';
+import MetaDefaultImage from '@/assets/images/meta-default.jpg';
+import avatar from '@/assets/images/avatar.jpeg';
+import type { seoSchemaWithoutImage } from '@/content.config';
+import astroConfig from 'astro.config.mjs';
+
+export type AuthorInfo = {
+  name: string;
+  avatar: any;
+  headline: string;
+  username?: string;
+  location?: string;
+  pronouns?: string;
+}
+
+export type Seo = z.infer<typeof seoSchemaWithoutImage> & {
+  image?: any;
+}
+
+type DefaultConfigurationType = {
+  baseUrl: string,
+  author: AuthorInfo;
+  seo: Seo;
+}
+
+export const DEFAULT_CONFIGURATION: DefaultConfigurationType = {
+  baseUrl: astroConfig.site || 'https://getcvfolio.com',
+  author: {
+    avatar,
+    name: 'Abyasa Bazla Wismaya',
+    headline: 'Cyber Security Enthusiast',
+    username: '@abysabzla',
+    location: 'Central Jakarta',
+    pronouns: 'He/Him',
+  },
+  seo: {
+    title: 'Abyasa Bazla Wismaya — Portofolio & Curicullum Vitae',
+    description: 'Abyasa Bazla Wismaya Cyber Security Enthusiast in Central Jakarta',
+    type: 'website',
+    image: MetaDefaultImage,
+    twitter: {
+      creator: '@cvfolio'
+    },
+    robots: 'noindex, nofollow',
+  }
+};
